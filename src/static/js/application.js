9e1565299a8e60dd68872734a4eb64410cc74e20
Ran Tavory <rantav@gmail.com>
2012-04-20 16:52:58 +0300

    Add syntax highlighting.

--- src/static/js/application.js
+++ src/static/js/application.js
@@ -18,10 +18,60 @@ $(document).ready(function() {
 function render_files(data) {
   html = [];
   for (var i = 0; i < data.files.length; ++i) {
-    html.push("<textarea>");
-    html.push(data.files[i].content);
-    html.push("</textarea>");
-
+    var file = data.files[i];
+    if (file.name == "file.thrift") {
+      continue;
+    }
+    html.push('<h2>');
+    html.push(file.name);
+    html.push('</h2>');
+    html.push("<pre class='brush: ");
+    html.push(data.language);
+    html.push("'>");
+    html.push(file.content);
+    html.push("</pre>");
   };
   $('#result_files').html(html.join(''));
+  syntax_highlight();
+}
+
+// initialize SyntaxHighlighter
+function syntax_highlight_path() {
+  var args = arguments, result = [];
+       
+  for(var i = 0; i < args.length; i++) {
+    result.push(args[i].replace('@', '/static/js/'));
+  }
+  return result
+};
+
+function syntax_highlight() {
+  SyntaxHighlighter.autoloader.apply(null, syntax_highlight_path(
+    'applescript            @shBrushAppleScript.js',
+    'actionscript3 as3      @shBrushAS3.js',
+    'bash shell             @shBrushBash.js',
+    'coldfusion cf          @shBrushColdFusion.js',
+    'cpp c                  @shBrushCpp.js',
+    'c# c-sharp csharp      @shBrushCSharp.js',
+    'css                    @shBrushCss.js',
+    'delphi pascal          @shBrushDelphi.js',
+    'diff patch pas         @shBrushDiff.js',
+    'erl erlang             @shBrushErlang.js',
+    'groovy                 @shBrushGroovy.js',
+    'java                   @shBrushJava.js',
+    'jfx javafx             @shBrushJavaFX.js',
+    'js jscript javascript  @shBrushJScript.js',
+    'perl pl                @shBrushPerl.js',
+    'php                    @shBrushPhp.js',
+    'text plain             @shBrushPlain.js',
+    'py python              @shBrushPython.js',
+    'ruby rails ror rb      @shBrushRuby.js',
+    'sass scss              @shBrushSass.js',
+    'scala                  @shBrushScala.js',
+    'sql                    @shBrushSql.js',
+    'vb vbnet               @shBrushVb.js',
+    'xml xhtml xslt html    @shBrushXml.js',
+    'java                   @shBrushJava.js'
+  ));
+  SyntaxHighlighter.all();
 }
