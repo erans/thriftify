7104264305e4d564c86b7470c5edf6a9f1afb543
Ran Tavory <rantav@gmail.com>
2012-04-20 23:11:50 +0300

    Use a single file for all syntax highlighting and compile that file from all the existing js files using closure

---
+++ compile_js.sh
@@ -0,0 +1,27 @@
+
+echo Compiling all Syntax Highlight files into one sh-all.js
+rm src/static/js/sh/sh-all.js
+#
+# jscompile uses google's closure compiler and is aliased as in:
+# alias jscompile="java -jar .../compiler.jar"
+#
+JS_COMPILE='java -jar /Users/ran/dev/obgit/trunk/devtools/bin/closure-js-compiler/compiler.jar'
+$JS_COMPILE --js src/static/js/sh/shCore.js\
+          --js src/static/js/sh/shBrushCpp.js\
+          --js src/static/js/sh/shBrushJava.js\
+          --js src/static/js/sh/shBrushRuby.js\
+          --js src/static/js/sh/shBrushAS3.js\
+          --js src/static/js/sh/shBrushDelphi.js\
+          --js src/static/js/sh/shBrushPerl.js\
+          --js src/static/js/sh/shBrushScala.js\
+          --js src/static/js/sh/shBrushPhp.js\
+          --js src/static/js/sh/shBrushErlang.js\
+          --js src/static/js/sh/shBrushPlain.js\
+          --js src/static/js/sh/shBrushVb.js\
+          --js src/static/js/sh/shBrushCSharp.js\
+          --js src/static/js/sh/shBrushGroovy.js\
+          --js src/static/js/sh/shBrushJScript.js\
+          --js src/static/js/sh/shBrushPython.js\
+          --js_output_file src/static/js/sh/sh-all.js\
+          --compilation_level WHITESPACE_ONLY
+ls -alh src/static/js/sh/sh-all.js
--- src/static/js/application.js
+++ src/static/js/application.js
@@ -35,43 +35,6 @@ function render_files(data) {
   syntax_highlight();
 }
 
-// initialize SyntaxHighlighter
-function syntax_highlight_path() {
-  var args = arguments, result = [];
-       
-  for(var i = 0; i < args.length; i++) {
-    result.push(args[i].replace('@', '/static/js/sh/'));
-  }
-  return result
-};
-
 function syntax_highlight() {
-  SyntaxHighlighter.autoloader.apply(null, syntax_highlight_path(
-    'applescript            @shBrushAppleScript.js',
-    'actionscript3 as3      @shBrushAS3.js',
-    'bash shell             @shBrushBash.js',
-    'coldfusion cf          @shBrushColdFusion.js',
-    'cpp c                  @shBrushCpp.js',
-    'c# c-sharp csharp      @shBrushCSharp.js',
-    'css                    @shBrushCss.js',
-    'delphi pascal          @shBrushDelphi.js',
-    'diff patch pas         @shBrushDiff.js',
-    'erl erlang             @shBrushErlang.js',
-    'groovy                 @shBrushGroovy.js',
-    'java                   @shBrushJava.js',
-    'jfx javafx             @shBrushJavaFX.js',
-    'js jscript javascript  @shBrushJScript.js',
-    'perl pl                @shBrushPerl.js',
-    'php                    @shBrushPhp.js',
-    'text plain             @shBrushPlain.js',
-    'py python              @shBrushPython.js',
-    'ruby rails ror rb      @shBrushRuby.js',
-    'sass scss              @shBrushSass.js',
-    'scala                  @shBrushScala.js',
-    'sql                    @shBrushSql.js',
-    'vb vbnet               @shBrushVb.js',
-    'xml xhtml xslt html    @shBrushXml.js',
-    'java                   @shBrushJava.js'
-  ));
-  SyntaxHighlighter.all();
+  SyntaxHighlighter.highlight();
 }
